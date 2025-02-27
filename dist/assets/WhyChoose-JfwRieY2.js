ce1e4c04ddb8fa198c0f45f91f32ae96bb0cab9c
KUNDAN <159764021+Kundan789@users.noreply.github.com>
2025-02-28 02:00:28 +0530

    Add files via upload

---
+++ dist/assets/WhyChoose-JfwRieY2.js
@@ -0,0 +1,16 @@
+import{c as a,j as e}from"./index-DYY2feWM.js";import{U as o}from"./user-check-BlD6vj_W.js";/**
+ * @license lucide-react v0.344.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const r=a("Clock",[["circle",{cx:"12",cy:"12",r:"10",key:"1mglay"}],["polyline",{points:"12 6 12 12 16 14",key:"68esgv"}]]);/**
+ * @license lucide-react v0.344.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const n=a("DollarSign",[["line",{x1:"12",x2:"12",y1:"2",y2:"22",key:"7eqyqh"}],["path",{d:"M17 5H9.5a3.5 3.5 0 0 0 0 7h5a3.5 3.5 0 0 1 0 7H6",key:"1b0p4s"}]]);/**
+ * @license lucide-react v0.344.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const c=a("Shield",[["path",{d:"M20 13c0 5-3.5 7.5-7.66 8.95a1 1 0 0 1-.67-.01C7.5 20.5 4 18 4 13V6a1 1 0 0 1 1-1c2 0 4.5-1.2 6.24-2.72a1.17 1.17 0 0 1 1.52 0C14.51 3.81 17 5 19 5a1 1 0 0 1 1 1z",key:"oel41y"}]]),l=({icon:s,title:t,description:i})=>e.jsxs("div",{className:"bg-white p-6 rounded-lg shadow-md hover:shadow-lg transition-all duration-300",children:[e.jsx("div",{className:"text-orange-500 mb-4",children:e.jsx(s,{size:32})}),e.jsx("h3",{className:"text-xl font-semibold mb-2",children:t}),e.jsx("p",{className:"text-gray-600",children:i})]}),x=()=>{const s=[{icon:c,title:"Confidential & Safe",description:"All your private information is safe with us"},{icon:n,title:"No Hidden Fee",description:"Everything is put before you with no hidden charges or conditions"},{icon:o,title:"Expert CA/CS Assistance",description:"Prompt support from our in-house expert professionals"},{icon:r,title:"Guaranteed Satisfaction",description:"We ensure that you stay 100% satisfied with our offered services"}];return e.jsx("section",{className:"py-16 bg-gray-50",children:e.jsxs("div",{className:"max-w-7xl mx-auto px-4 sm:px-6 lg:px-8",children:[e.jsxs("div",{className:"text-center mb-12",children:[e.jsx("span",{className:"text-orange-500 uppercase tracking-wider font-medium",children:"WHY CHOOSE US"}),e.jsx("h2",{className:"text-3xl font-bold text-gray-900 mt-2",children:"Why Choose Register Karo"}),e.jsx("p",{className:"mt-4 text-gray-600 max-w-2xl mx-auto",children:"It is with consistent services and results that build trust with the people and that in turn help us to serve the business better."})]}),e.jsx("div",{className:"grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8",children:s.map((t,i)=>e.jsx(l,{...t},i))})]})})};export{x as default};
